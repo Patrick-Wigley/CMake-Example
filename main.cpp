e7a9f62d76e47a9e28b92c76f40abb2a5d5e2d4a
Patrick <patrickwigley2@gmail.com>
2023-05-30 16:30:39 +0100

    Added contents from local environment

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Dependencies\GLEW\Include\GL\glew.h"
+#include "Dependencies\GLFW\Include\GLFW\glfw3.h"
+#include "Dependencies\\GLM\\glm\\glm.hpp"
+#include "Dependencies\\GLM\\glm\\gtc\\type_ptr.hpp"
+
+
+
+int main()
+{
+    printf("%s", "Hello World. I am adding a new change to code");
+    return 0;
+}
