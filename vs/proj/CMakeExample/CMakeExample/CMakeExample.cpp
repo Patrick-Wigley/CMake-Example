8a74b1e8cfdd374f1cfe0402742444e3316b3938
Patrick <patrickwigley2@gmail.com>
2023-06-27 13:10:31 +0100

    Initial commit for cmake c++ Visual Studio Project

---
+++ vs/proj/CMakeExample/CMakeExample/CMakeExample.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeExample.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeExample.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
